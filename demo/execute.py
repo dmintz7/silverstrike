8b8022a798e962f6a823da9580b2a47d417e80a6
dmintz7 <dmintz37@gmail.com>
2020-11-13 13:00:07 -0500

    Update scheduler

--- demo/execute.py
+++ demo/execute.py
@@ -2,14 +2,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 from datetime import datetime
 
-from silverstrike.mohair.emails import queue
+from silverstrike.mohair.emails import check_new
 from silverstrike.mohair.utils import remove_unused_accounts
 from silverstrike.mohair.slack_bot import start_bot
 
 scheduler = BackgroundScheduler()
 scheduler.configure(timezone=utc)
 
-scheduler.add_job(queue, 'interval', minutes=2, next_run_time=datetime.now())
+scheduler.add_job(check_new, 'interval', minutes=2, next_run_time=datetime.now())
 scheduler.add_job(remove_unused_accounts, 'interval', hours=1, next_run_time=datetime.now())
 scheduler.add_job(start_bot)
 scheduler.start()
