0a393735342eb9312726250125115c57da7a0261
dmintz7 <dmintz37@gmail.com>
2021-02-27 14:20:42 -0500

    Update execute.py
    Use timezone from setting for scheduler

--- slack/execute.py
+++ slack/execute.py
@@ -1,9 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 from slack.bot import start_bot
+from django.conf import settings
 
 scheduler = BackgroundScheduler()
-scheduler.configure(timezone=utc)
+scheduler.configure(timezone=settings.TIME_ZONE)
 
 scheduler.add_job(start_bot)
 scheduler.start()
