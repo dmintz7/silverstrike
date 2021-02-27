cbef7de825d6dde87c9cd764ad9ca612edd835cf
dmintz7 <dmintz37@gmail.com>
2021-02-26 23:06:13 -0500

    Add more options to charts

--- silverstrike/views/charts.py
+++ silverstrike/views/charts.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
 from silverstrike.lib import last_day_of_month
-
+from silverstrike.models import Split
 
 class ChartView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'silverstrike/charts.html'
@@ -18,6 +18,12 @@ class ChartView(LoginRequiredMixin, generic.TemplateView):
         context['minus_3_months'] = date.today() - relativedelta(months=3)
         context['minus_6_months'] = date.today() - relativedelta(months=6)
         context['minus_12_months'] = date.today() - relativedelta(years=1)
+        context['minus_24_months'] = date.today() - relativedelta(years=2)
+        context['minus_36_months'] = date.today() - relativedelta(years=3)
+        context['minus_48_months'] = date.today() - relativedelta(years=4)
+        context['minus_60_months'] = date.today() - relativedelta(years=5)
+        
+        context['max'] = Split.objects.order_by('date').first().date
 
         context['first_day_of_month'] = date.today().replace(day=1)
         context['last_day_of_month'] = last_day_of_month(date.today())
