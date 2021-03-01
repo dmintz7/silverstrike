39ca17e107471703d12760769701adcce67e8789
dmintz7 <dmintz37@gmail.com>
2021-03-01 00:20:18 -0500

    Update docker-entrypoint.sh
    Emoji giving random error until different version is installed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,6 +13,34 @@ sed -i 's/default_label/'$EMAIL_LABEL'/g' '/app/download.fetchmailrc'
 # Create admin user if no user exists
 python3 /app/initialize.py
 
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+
+# TEMP FIX FOR EMOJI
+
+version=$(pip3 freeze | grep emoji)
+if python3 -c "import emoji" &> /dev/null; then
+	echo "Emoji Can Load"
+else
+	echo "Emoji Errored Loading- Current Version "$version
+	if [[ "$version" == *1.2.0* ]]; then
+	  pip3 install emoji==0.6.0
+	else
+	  pip3 install emoji==1.2.0
+	fi
+fi
+
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+
 # Start Server
 echo "Starting Server"
 uwsgi --ini /app/silverstrike.ini
