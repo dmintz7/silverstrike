f2a195ee3db887112ef728f87fc38dffcab586af
dmintz7 <dmintz37@gmail.com>
2021-03-01 00:20:43 -0500

    Update execute.py
    Add settings timezone to email scheduler

--- emails/execute.py
+++ emails/execute.py
@@ -1,11 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 from datetime import datetime
+from django.conf import settings
 
 from emails.process import check_new
 
 scheduler = BackgroundScheduler()
-scheduler.configure(timezone=utc)
+scheduler.configure(timezone=settings.TIME_ZONE)
 
 scheduler.add_job(check_new, 'interval', minutes=2, next_run_time=datetime.now())
 scheduler.start()
